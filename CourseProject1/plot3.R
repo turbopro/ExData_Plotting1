e3a2554815a172f0840a983c7ffa48cf99f52f2e
turbopro <dantepoe@gmail.com>
2014-11-07 07:02:49 -0500

    Date: Nov 07 2014 Updated:
    	modified:   plot2.R
    	modified:   plot2.png
    	modified:   plot3.R
    	modified:   plot3.png
    	modified:   plot4.R
    	modified:   plot4.png

--- CourseProject1/plot3.R
+++ CourseProject1/plot3.R
@@ -1,24 +1,28 @@
-plot3 <- function(file="household_power_consumption.txt") {
+# this file -- plot3.R
+# function definition to save Plot 3, "Energy sub metering," to image file
+# calls "readSubsetData()" to read in subset of data required
+
+plot3 <- function() {
      # check for dataframe
-     # if not exists, read in subset of the dataset required
-     # store in dataframe 'tempDF' as global object
-     # else, print PLOT 3
+     # if not exists, call "readSubsetData()"
      if(!exists("tempDF")) {
-          # read in data using 'readSusbsetFile()'
-          tempDF <<- readSubsetFile(file)
-          
-          # then plot PLOT 3
-          # open file to save image
-          png("plot3.png", width=480, height=480)
-          
-          plot(tempDF$DT, tempDF$Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="datetime")
-          dev.off()
-     } else {          
-          # plot PLOT 3
-          # open file to save image
-          png("plot3.png", width=480, height=480)
-          
-          plot(tempDF$DT, tempDF$Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="datetime")
-          dev.off()
+          # read in data and store in 'tempDF' global object (no need to recompute)
+          tempDF <<- readSubsetData()
      }
+     
+     # open file to save image
+     png("plot3.png", width=480, height=480)
+          
+     # set y-axis range for the three variables
+     yrange <- (range(c(tempDF$Sub_metering_1, tempDF$Sub_metering_2, tempDF$Sub_metering_3)))
+          
+     # plot Plot 3
+     plot(tempDF$DT, tempDF$Sub_metering_1, type="l", ylim=yrange, xlab="", ylab="Energy sub metering")
+     lines(tempDF$DT, tempDF$Sub_metering_2, type="l", ylim=yrange, xlab="", ylab="Energy sub metering", col="RED")
+     lines(tempDF$DT, tempDF$Sub_metering_3, type="l", ylim=yrange, xlab="", ylab="Energy sub metering", col="BLUE")
+     legend(x="topright", legend=c("Submetering_1", "Submetering_2", "Submetering_3"), col=c("black", "red", "blue"), 
+            lty=1, seg.len=1, xjust=0, cex=0.85, x.intersp=0.5)
+          
+     # close png device
+     dev.off()
 }
